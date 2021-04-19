cd678125567b3c25597642d01862b5f21a58b7dd
psilva009 <marips0202@gmail.com>
2021-04-19 13:00:53 -0300

    docs: Frontend improvements

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,9 +1,20 @@
-from app import app, render_template
+from app import app, render_template, redirect, url_for, request
 
 from app.models.formsModel import InputDataForm
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
-@app.route('/')
+@app.route('/', methods=["GET","POST"])
 def index():
-    form = InputDataForm();
+    form = InputDataForm()
+
+    if form.validate_on_submit():
+        print(form)
+    else:
+        print(form.errors)
+
     return render_template('index.html', form=form)
+  
+
